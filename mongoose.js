7b7d8593f60df7cb49a5a21d6bce31fbbd6e4a37
SandeepRana1001 <38207659+SandeepRana1001@users.noreply.github.com>
2022-01-13 20:20:59 +0530

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const Product = require('./models/product')
-const url = 'mongodb+srv://Sandeep_Rana:Ran122015HR@cluster0.pjnif.mongodb.net/products_test?retryWrites=true&w=majority'
+const url = 'mongodb+srv://Sandeep_Rana:<password>@cluster0.pjnif.mongodb.net/products_test?retryWrites=true&w=majority' // Your MongoDB Password
 
-mongoose.connect(url, {
+mongoose.connect(url, {             // Setting up connection
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('connection successfull'))
     .catch((err) => console.log(err))
 
-const createProduct = async (req, res, next) => {
+const createProduct = async (req, res, next) => {           // create  a new product
     const createdProduct = new Product({
         name: req.body.name,
         price: req.body.price,
@@ -17,10 +17,10 @@ const createProduct = async (req, res, next) => {
     res.json(result)
 }
 
-const getProduct = async (req, res, next) => {
+const getProducts = async (req, res, next) => {      // get all product
     const products = await Product.find().exec()
     res.json(products)
 }
 
 exports.createProduct = createProduct
-exports.getProduct = getProduct 
+exports.getProducts = getProducts 
